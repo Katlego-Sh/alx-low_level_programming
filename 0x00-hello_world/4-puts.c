b353b0e35ffd1f6dadf9b0dbdd0a0d23dc668980
Katlego-Sh <blessinshab@gmail.com>
2023-03-21 06:29:43 -0700

    my first C without script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - the starting section
+*return - a 0 return is correct
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
