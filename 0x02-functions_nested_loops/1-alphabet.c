6b8721fd7fa98ce8cb4a1607ead56066c0f3286a
Katlego-Sh <blessinshab@gmail.com>
2023-03-22 09:52:19 -0700

    printing letters till z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char let;
+for (let = 'a'; let <= 'z'; let++)
+_putchar(let);
+_putchar('\n');
+}
